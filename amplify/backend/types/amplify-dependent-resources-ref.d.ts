b584ef3586fc06b864eb3292d172774b12a9a637
Carlos Feliz <carlosjfeliz7@gmail.com>
2023-09-19 00:11:09 -0400

    initial setup Nextjs, styled-components, amplify AWS

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quotegen": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "quotegenec7e7700": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "quotesHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
+import awsExports from "@/src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
